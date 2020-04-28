c181710616b311b2894b6b3c96ffb586e5719761
Gilbert Hsu (via Travis CI) <45667486+sketchbook21@users.noreply.github.com>
2020-04-28 15:24:15 +0000

    Deploy sketchbook21/iCarb to github.com/sketchbook21/iCarb.git:gh-pages

---
+++ precache-manifest.e4b0a4f54a56be6e61987654a58d31e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e39254a0f19fadcfc47ad18fe3ded21b",
+    "url": "/iCarb/index.html"
+  },
+  {
+    "revision": "9c5ec26c7085499ae5b1",
+    "url": "/iCarb/static/css/main.2ddd170b.chunk.css"
+  },
+  {
+    "revision": "85293121b313f0342cd2",
+    "url": "/iCarb/static/js/2.234d76e0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/iCarb/static/js/2.234d76e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c5ec26c7085499ae5b1",
+    "url": "/iCarb/static/js/main.d523efb5.chunk.js"
+  },
+  {
+    "revision": "2253efceb9ecd79c91e6",
+    "url": "/iCarb/static/js/runtime-main.d7b2b667.js"
+  },
+  {
+    "revision": "9d3c38e8d327a15348bb66e676820877",
+    "url": "/iCarb/static/media/hero-final.9d3c38e8.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d32e3aa1ebe5824d55aa659b83e4013.js"
+  "/iCarb/precache-manifest.e4b0a4f54a56be6e61987654a58d31e6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iCarb/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
