f0266368fb6ae8b5138558edef01d0dae8ca7532
Gilbert Hsu <45667486+sketchbook21@users.noreply.github.com>
2020-04-29 00:04:19 -0400

    Revert "wip"

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React from "react";
 import "./App.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Provider } from "react-redux";
 import Home from "./components/Home";
 import Order from "./components/Order";
 import NotFound from "./components/NotFound";
 import Nav from "./components/Nav";
 import Checkout from "./components/Checkout";
 
-const App = (props) => {
+function App() {
   return (
-    <Provider store={props.store}>
-      <Router>
-        <Route component={Nav} path="/icarb" />
-        <Switch>
-          <Route component={Home} exact path="/icarb" />
-          <Route component={Order} exact path="/icarb/pizza/:id" />
-          <Route component={Checkout} exact path="/icarb/checkout" />
-          <Route component={NotFound} path="/icarb/" />
-        </Switch>
-      </Router>
-    </Provider>
+    <Router>
+      <Route component={Nav} path="/icarb" />
+      <Switch>
+        <Route component={Home} exact path="/icarb" />
+        <Route component={Order} exact path="/icarb/order/:id/item/:id" />
+        <Route component={Checkout} exact path="/icarb/checkout/:id" />
+        <Route component={NotFound} path="/icarb/" />
+      </Switch>
+    </Router>
   );
-};
+}
 
 export default App;
--- src/components/containers/SubtotalContainer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SubtotalContainer = (props) => {
-  return <div>Hello from SubtotalContainer</div>;
-};
-
-export default SubtotalContainer;
