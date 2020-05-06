848c35235254bc7f4078f7f792e80ceb81134782
sketchbook21 <gilbert.hsu2@gmail.com>
2020-05-06 10:13:49 -0400

    hookup add another pizza button

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { useHistory } from "react-router";
 import { connect } from "react-redux";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import CheckoutItem from "./tiles/CheckoutItem";
+import { resetOrder } from "../modules/pizzas";
 
-const Checkout = ({ cart, cartTotal, displayCartTotal }) => {
+const Checkout = ({ cart, displayCartTotal, resetOrder }) => {
+  const history = useHistory();
+  const handleAddAnother = () => {
+    resetOrder();
+    history.push("/icarb/pizza/2");
+  };
   const displayCart = cart.map((pizza) => {
     return (
       <CheckoutItem
@@ -20,13 +27,30 @@ const Checkout = ({ cart, cartTotal, displayCartTotal }) => {
           Your order total is {displayCartTotal}
         </Col>
         <Col className="d-flex justify-content-center" md={12}>
-          Someone is hungry.
+          Your friends are gonna be so jelly.
         </Col>
       </Row>
       {displayCart}
       <Row className="d-flex justify-content-between fs-3 fw-7 cart-total">
         <div>Total</div>
         <div>{displayCartTotal}</div>
+      </Row>
+      <Row className="d-flex justify-content-between">
+        <Button
+          variant="outline-secondary"
+          className=" mt-3 mb-5"
+          style={{ width: "20vw" }}
+          onClick={handleAddAnother}
+        >
+          Add Another Pizza
+        </Button>
+        <Button
+          variant="primary"
+          className=" mt-3 mb-5"
+          style={{ width: "20vw" }}
+        >
+          Order Now
+        </Button>
       </Row>
     </Container>
   );
@@ -35,9 +59,14 @@ const Checkout = ({ cart, cartTotal, displayCartTotal }) => {
 const mapStateToProps = (state) => {
   return {
     cart: state.pizzas.cart,
-    cartTotal: state.pizzas.cartTotal,
     displayCartTotal: state.pizzas.displayCartTotal,
   };
 };
 
-export default connect(mapStateToProps, null)(Checkout);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    resetOrder: () => dispatch(resetOrder()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
